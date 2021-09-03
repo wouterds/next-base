1c8615add950fcef06f02b3f2746c465dc1046fc
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2021-09-03 19:54:52 +0200

    :sparkles: Hello world

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import 'styles/global.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-export default MyApp
+import type { AppProps } from 'next/app';
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
